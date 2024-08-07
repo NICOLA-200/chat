5cae7260a6fb48196039b5257c75f666454bdd00
NICOLA-200 <fatepepe66@gmail.com>
2024-08-07 18:17:19 +0200

    url updated

--- chatBuddy/base/urls.py
+++ chatBuddy/base/urls.py
@@ -2,16 +2,21 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('login/', views.loginPage, name='login'),
-     path('', views.room, name="room"),
-     path('room/<str:pk>/', views.home, name="home"),
-     path('create-room/', views.createRoom, name="create-room"),
+    path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
 
-     path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
-     path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
-     path('create-room/', views.createRoom, name="create-room"),
-     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+    path('', views.home, name="home"),
+    path('room/<str:pk>/', views.room, name="room"),
+    path('profile/<str:pk>/', views.userProfile, name="user-profile"),
 
-     path('topics/', views.topicsPage, name="topics"),
-     path('activity/', views.activityPage, name="activity"),
+    path('create-room/', views.createRoom, name="create-room"),
+    path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
+    path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
+    path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+
+#     path('update-user/', views.updateUser, name="update-user"),
+
+#     path('topics/', views.topicsPage, name="topics"),
+#     path('activity/', views.activityPage, name="activity"),
 ]
