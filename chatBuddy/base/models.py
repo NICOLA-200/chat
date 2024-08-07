41eb7cbce6f4d297a2d18620f056ac580686eb00
NICOLA-200 <fatepepe66@gmail.com>
2024-08-07 17:33:55 +0200

    room udated

--- chatBuddy/base/models.py
+++ chatBuddy/base/models.py
@@ -16,19 +16,20 @@ class Topic(models.Model):
 
 
 class Room(models.Model):
-     #host = 
-     #topic =
-     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
-     name = models.CharField(max_length=200)
-     description  =  models.TextField(null=True, blank=True)
-     #participants = 
-     updated = models.DateTimeField(auto_now=True)
-     created = models.DateTimeField(auto_now_add=True)
+    host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(
+        User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
 
-     def __str__(self):
-          return self.name
+    def __str__(self):
+        return self.name
      
 
 
