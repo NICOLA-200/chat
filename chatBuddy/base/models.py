eb5ff03055480873e809a85863b0dd03e15f0861
NICOLA-200 <fatepepe66@gmail.com>
2024-08-07 17:32:10 +0200

    particitant updated

--- chatBuddy/base/models.py
+++ chatBuddy/base/models.py
@@ -21,7 +21,7 @@ class Room(models.Model):
      host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
      topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
      name = models.CharField(max_length=200)
-     description  =  models. TextField(null=True, blank=True)
+     description  =  models.TextField(null=True, blank=True)
      #participants = 
      updated = models.DateTimeField(auto_now=True)
      created = models.DateTimeField(auto_now_add=True)
@@ -31,6 +31,9 @@ class Room(models.Model):
           return self.name
      
 
+
+     
+
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
@@ -38,8 +41,10 @@ class Message(models.Model):
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['-updated', '-created']
+
     def __str__(self):
-        return self.body[0:50]
-     
+        return self.body[0:50]     
       
       
