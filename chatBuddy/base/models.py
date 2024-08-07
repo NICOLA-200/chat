9c390307b7f794f9a1fec041add63362a65b5dba
NICOLA-200 <fatepepe66@gmail.com>
2024-08-07 15:31:03 +0200

    user changed

--- chatBuddy/base/models.py
+++ chatBuddy/base/models.py
@@ -1,7 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=200)
