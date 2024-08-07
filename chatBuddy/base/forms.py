626d55288fd95837cfa4bf0acd022b3d79bbb9f0
NICOLA-200 <fatepepe66@gmail.com>
2024-08-07 18:37:27 +0200

    forms updated

--- chatBuddy/base/forms.py
+++ chatBuddy/base/forms.py
@@ -1,5 +1,6 @@
 from django.forms import ModelForm
-from .models import Room
+from .models import Room, User
+from django.contrib.auth.forms import UserCreationForm
 
 
 
@@ -7,3 +8,16 @@ class RoomForm(ModelForm):
      class Meta:
           model = Room
           fields = '__all__'
+
+
+class MyUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['name', 'username', 'email', 'password1', 'password2']
+
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['avatar', 'name', 'username', 'email', 'bio']
+
