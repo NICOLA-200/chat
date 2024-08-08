91e248e556191f4a8e92b153a433eab1392bc4fc
NICOLA-200 <fatepepe66@gmail.com>
2024-08-08 12:07:45 +0200

    database fixed

--- chatBuddy/base/forms.py
+++ chatBuddy/base/forms.py
@@ -8,6 +8,7 @@ class RoomForm(ModelForm):
      class Meta:
           model = Room
           fields = '__all__'
+          exclude = ['host', 'participants']
 
 
 class MyUserCreationForm(UserCreationForm):
